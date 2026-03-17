683825769f56830ef1da0594a887e7dc4f348655
Kaan <134944208+kaanthealien@users.noreply.github.com>
2026-03-17 21:41:20 +0300

    file update

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ from app import asistan_cevapla # app.py'daki fonksiyonu çağırıyoruz
 
 st.set_page_config(page_title="DevOps AI Assistant", page_icon="🤖")
 
-st.title("🚀 SmartDevOps Knowledge Base")
+st.title("DevOps Asistan DEMOSU")
 st.markdown("Azure AI & RAG mimarisi ile güçlendirilmiş teknik asistan.")
 
 if "messages" not in st.session_state:
@@ -15,7 +15,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Kullanıcıdan soru al
-if prompt := st.chat_input("Hangi dokümanı merak ediyorsun?"):
+if prompt := st.chat_input("Neyi Merak Ediyorsun ?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
